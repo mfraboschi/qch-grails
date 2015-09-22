05792cfd4a2708190c2ae6cc300d03174f06b0f9
GuibuA <guibuadrian@gmail.com>
2015-09-22 15:24:05 -0300

    Lo comentado lo agregue yo

--- grails-app/domain/qch/Ingrediente.groovy
+++ grails-app/domain/qch/Ingrediente.groovy
@@ -2,7 +2,8 @@ package qch
 
 class Ingrediente {
     String nombre
-
+    String contraindicaciones  //:DD
+    Integer ubEnPiramide  //  La posicion enla piramide
     static mapping = {
         id name: 'nombre'
     }
