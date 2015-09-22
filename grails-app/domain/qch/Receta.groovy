3a55e678fdf7fe43b1bd5f6f20ed6f5b441dc398
GuibuA <guibuadrian@gmail.com>
2015-09-22 15:19:31 -0300

    Donde hay comentarios lo agregue yo

--- grails-app/domain/qch/Receta.groovy
+++ grails-app/domain/qch/Receta.groovy
@@ -4,13 +4,13 @@ class Receta {
     String nombre
     Integer dificultad
     Integer caloriasTotal
-    Usuario dueño
+    Usuario dueño          //Lo llame dueño
     String temporada
-    Integer cantVisitas
+    Integer cantVisitas   //Acumular las veces que un Usuario visito la receta
 
     static hasMany = [ingredientes: IngredienteReceta,
                       condimentos: CondimentoReceta,
-                      categorias: Categoria]
+                      categorias: Categoria]   //Cena, almuerzo, ...
 
     static constraints = {
         usuario nullable: true
@@ -19,4 +19,6 @@ class Receta {
     static mapping = {
 
     }
+    
+    //    Hay q agregarle los procedimientos. Son hasta 5
 }
