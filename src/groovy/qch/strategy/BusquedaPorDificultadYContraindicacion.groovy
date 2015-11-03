7e4a240a76867ed81e49356ceeac9604786784c8
GuibuA <guibuadrian@gmail.com>
2015-11-03 18:29:59 -0300

    Arreglo busqueda por dificultad y contraindicacion

--- src/groovy/qch/strategy/BusquedaPorDificultadYContraindicacion.groovy
+++ src/groovy/qch/strategy/BusquedaPorDificultadYContraindicacion.groovy
@@ -1,13 +1,38 @@
 package qch.strategy
 
+import qch.enums.CondicionPreexistente
+import qch.enums.Dieta
+import qch.enums.Dificultad
+import qch.receta.Contraindicacion
 import qch.receta.Receta
 
 /**
  * Created by mfraboschi on 13/10/15.
  */
+/*
 class BusquedaPorDificultadYContraindicacion implements EstrategiaBusqueda {
     @Override
     def obtenerResultados(Map parametros) {
         Receta.findAllByContraindicacionesInListAndDificultad([parametros.contraindicacion], parametros.dificultad)
     }
 }
+*/
+
+class BusquedaPorDificultadYContraindicacion implements EstrategiaBusqueda {
+    @Override
+    def obtenerResultados(Map parametros) {
+
+        def criteria = Contraindicacion.createCriteria()
+
+        def result = criteria.list {
+            "receta" {
+                eq "dificultad", Dificultad.valueOf(parametros.dificultad)
+            }
+            eq 'condicionPreexistente', CondicionPreexistente.valueOf(parametros.contraindicacion)
+        }
+
+        return result.collect { it.receta }
+    }
+}
+
+	
