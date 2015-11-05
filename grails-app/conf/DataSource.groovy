952129fdf4f80bd7296f7536dc1346060c293966
Matias Fraboschi <matias.fraboschi@mercadolibre.com.ar>
2015-11-04 22:37:28 -0300

    configuracion para usar el mysql

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -22,6 +22,16 @@ environments {
             url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
             logSql = true
         }
+        /*
+         dataSource {
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://127.0.0.1:3306/qch"
+            username = "root"
+            password = ""
+            dialect=org.hibernate.dialect.MySQLDialect.class
+            pooled = false
+        }
+         */
     }
     test {
         dataSource {
