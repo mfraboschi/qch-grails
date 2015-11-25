4fe416c830c265f0b30416fce3b12179277970eb
GuibuA <guibuadrian@gmail.com>
2015-11-25 16:37:00 -0300

    Agrego otra busqueda

---
+++ src/groovy/qch/strategy/BusquedaPorDificultadDietaTemporadaYCategoria.groovy
@@ -0,0 +1,43 @@
+package qch.strategy
+
+import qch.enums.CondicionPreexistente
+import qch.enums.Dieta
+import qch.enums.Dificultad
+import qch.enums.CategoriaEnum
+import qch.enums.Temporada
+import qch.receta.TemporadaReceta
+import qch.receta.Contraindicacion
+import qch.receta.Receta
+import qch.receta.Categoria
+import qch.usuario.Grupo
+import qch.usuario.Usuario
+
+/**
+ * Created by mfraboschi on 13/10/15.
+ */
+class BusquedaPorDificultadDietaTemporadaYCategoria implements EstrategiaBusqueda {
+	@Override
+	def obtenerResultados(Usuario usuario, Map parametros)
+	{
+        def creadores = Grupo.obtenerUsuariosDeSusGrupos(usuario)
+
+        Receta.withCriteria {
+            or {
+                isNull("creador")
+                'in' "creador", creadores
+            }
+			and {
+				eq "dieta", Dieta.valueOf(parametros.dieta)
+				eq "dificultad", Dificultad.valueOf(parametros.dificultad)
+				
+				temporadas {
+					eq "temporada", Temporada.valueOf(parametros.temporada)
+				}
+				categorias {
+					eq "nombre", CategoriaEnum.valueOf(parametros.categoria)
+				}
+			}
+
+		}
+	}
+}
