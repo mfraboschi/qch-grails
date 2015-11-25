c46353366e48fbb81f5d7d3b36926998ed13391e
Joaro92 <joaro_92@yahoo.com.ar>
2015-11-25 17:40:54 -0300

    bugfix a la busqueda con usuarios nuevos

--- grails-app/domain/qch/usuario/Grupo.groovy
+++ grails-app/domain/qch/usuario/Grupo.groovy
@@ -4,11 +4,11 @@ class Grupo {
     String nombre
     String creadorId
 	String descripcion
-	
+
     static hasMany = [usuarios: Usuario]
 
     static mapping = { usuarios lazy: false }
-	
+
 	public boolean pertenece(Usuario usuario) {
 		 (this.usuarios.asList().contains(usuario))
 	}
@@ -16,7 +16,7 @@ class Grupo {
     public borrar() {
         this.delete(flush: true)
     }
-	
+
 	public crearGrupo(guardarGrupo, Usuario usuario)
 	{
 		this.creadorId = usuario.nickName
@@ -25,13 +25,13 @@ class Grupo {
 		this.addToUsuarios(usuario)
 		this.save()
 	}
-	
+
 	public agregarA(Usuario usuario)
 	{
 		this.addToUsuarios(usuario)
 		this.save()
 	}
-	
+
 	public removerA(Usuario usuario)
 	{
 		this.removeFromUsuarios(usuario)
@@ -40,12 +40,12 @@ class Grupo {
 
     public static obtenerUsuariosDeSusGrupos(Usuario usuario) {
         Set usuarios = new HashSet()
-        usuario.grupos.each { Grupo grupo ->
-            grupo.usuarios.each {
+        usuario.grupos.each {
+            Grupo grupo -> grupo.usuarios.each {
                 usuarios.add(it.nickName)
             }
         }
-
+        usuarios.add(usuario.nickName)
         return usuarios
     }
 }
